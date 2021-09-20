b57391ead5d6f927cc152b841030ca860f0fb524
Martin Mantler <mmantler@gmail.com>
2021-09-20 16:04:37 -0500

    Week 3 homework final

---
+++ homework02/README.md
@@ -0,0 +1,6 @@
+# My Name is Martin Mantler
+## I am person knowledge of Full Stack web development
+1. Learn HTML
+2. Learn CSS
+3. Learn Mongo DB
+4. Connect HTML with database
---
+++ homework02/test.js
@@ -0,0 +1,40 @@
+// // create a variable referencing a primitive value
+  let y = 3;
+
+// // "pass" the value to x
+// // x and y === 3, but each is referencing a different copy of the value
+let x = x + 1;
+
+// // function to mutate
+// const addOne = (num) => num + 1;
+
+// // mutate y
+// y = addOne(y);//
+// console.log("y="+y);
+// // now, y === 4
+
+// // x is still === 3
+//  console.log("x="+x);
+// comparing and passing by reference and value
+// ? determine whether each log will print true or false
+
+// console.log(4 === 4);
+// console.log("Hello, world" === "Hello, world");
+
+// let x = false;
+// let y = x;
+// console.log(x === y);
+// console.log("x && Y =" + (x&&y))
+// // x = !x;
+// // console.log(x === y);
+
+// console.log([1, 2, "c"] === [1, 2, "c"]);
+// let x = {};
+// console.log( x === x )
+// console.log({} === {});
+
+// y = { id: 1 };
+// x = y;
+// console.log(x === y);
+// y.id += 1;
+// console.log(x.id === y.id);
